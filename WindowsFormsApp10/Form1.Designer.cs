24474ee309b87a50eed75239f7027e00872766e2
ericool <ermak47@list.ru>
2025-01-20 23:09:17 +0300

    1,0

--- WindowsFormsApp10/Form1.Designer.cs
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -309,7 +309,7 @@
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(578, 328);
+            this.ClientSize = new System.Drawing.Size(577, 321);
             this.Controls.Add(this.btnRemoveAllStreamers);
             this.Controls.Add(this.lblCountdown);
             this.Controls.Add(this.btnCheckAllCategories);
